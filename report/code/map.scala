86e137c6639850725d2b4537f11e42f72644d30d
Ólafur Páll Geirsson <olafurpg@gmail.com>
2015-06-07 23:33:43 +0200

    WIP

--- report/code/map.scala
+++ report/code/map.scala
@@ -2,5 +2,5 @@
 def map[F, G, T](f: E => F)
   (implicit shape: Shape[_ <: FlatShapeLevel, F, T, G]): Query[G, T, C]
 // slick.direct
-def map[F](f: E => F): Query[F, C]
+def map[F](f: T => F): Query[F, C]
 
